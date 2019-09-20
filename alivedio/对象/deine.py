182c35886240976e1b6b5f55a8ee75d695e5cf1d
vancekq <zhankq@163.com>
2019-09-20 08:16:08 +0800

    更新目录结构

---
+++ alivedio/对象/deine.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self,name):
+        self.name = name
+    def say_hi(self):
+        print('name')
+
+one = Person('zhangsan')
+one.say_hi();
---
+++ website/《python36手册》/9.类/init.py
@@ -0,0 +1,11 @@
+class initdemo:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def getData(self,name):
+        return  self.name
+
+
+f = initdemo('aa','bbb')
+print(f.y)
