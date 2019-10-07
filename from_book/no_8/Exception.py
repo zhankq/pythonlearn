23d496948629a4de1fad7a86456bd22c2a6db5bd
vancekq <zhankq@163.com>
2019-10-08 07:51:12 +0800

    python异常处理【1】

---
+++ from_book/no_8/Exception.py
@@ -0,0 +1,6 @@
+try:
+    x=int(input('Enter the first number: '))
+    y=int(input('Enter the second number: '))
+    print(x/y)
+except ZeroDivisionError:
+    print("The second number can't be zero !")
---
+++ from_book/no_8/README.md
@@ -0,0 +1,4 @@
+异常处理
+自定义异常类[继承异常类]
+class SommeCustomException(Exception):
+    pass
