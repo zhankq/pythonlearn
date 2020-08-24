46d9783a54f397806b4ad45ebb8667140c45c5a3
vancekq <zhankq@163.com>
2020-08-24 23:58:06 +0800

    更新内容

--- flask/quickstart_request.py
+++ flask/quickstart_request.py
@@ -1,1 +1,46 @@
-from flask import request
+from flask import Flask,escape,url_for,request,render_template,session,abort, redirect,jsonify
+app = Flask(__name__)
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+app.logger.debug('A value for debugging')
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+
+@app.route("/me")
+def me_api():
+    user = get_current_user()
+    return {
+        "username": user.username,
+        "theme": user.theme,
+        "image": url_for("user_image", filename=user.image),
+    }
+
+def get_current_user():
+    return jsonify({"username":"zhangsan","theme":"China","image":"https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"})
+
