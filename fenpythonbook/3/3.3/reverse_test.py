6f8cc38e8e09f50d02f842a50936e8bfc291fe55
vancekq <zhankq@163.com>
2020-10-10 08:07:24 +0800

    更新上传的信息

---
+++ fenpythonbook/3/3.3/reverse_test.py
@@ -0,0 +1,6 @@
+#所有元素，顺序反转
+
+a_list = list(range(1,8))
+a_list.reverse()
+
+print(a_list)
---
+++ fenpythonbook/3/3.3/tuple_test.py
@@ -0,0 +1,15 @@
+a_list = ["crazyit",20,-1.2]
+
+# 将表  转换 成元组
+a_tuple = tuple(a_list)
+print(a_tuple)
+#使用 range()函数 创建区间 range对象
+a_range = range(1,5)
+print(a_range)
+
+b_tuple = tuple(a_range)
+print(b_tuple)
+
+c_tuple = tuple(range(4,20,3))
+
+print(c_tuple)
