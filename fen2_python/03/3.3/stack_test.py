142720fa2d0d9523e4749cfbe44ee90b8816ba95
zhankq@163.com <zhankq@163.com>
2021-06-03 19:42:23 +0800

    .

--- fen2_python/03/3.3/stack_test.py
+++ fen2_python/03/3.3/stack_test.py
@@ -1,4 +1,19 @@
 stack = []
+#向栈中入栈3个元素
+stack.append("fkit")
+stack.append("crazyit")
+stack.append("Charlie")
+
+print(stack)
+#第一出栈：最后的入栈的出去
+print(stack.pop())
+print(stack)
+#再次出栈
+print(stack.pop())
+print(stack)
+
+'''
+stack = []
 #向栈中加3个元素
 stack.append("fkit")
 stack.append("crazyit")
@@ -9,3 +24,4 @@ print(stack.pop())
 print(stack)
 print(stack.pop())
 print(stack)
+'''
