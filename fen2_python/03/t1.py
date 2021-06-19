25eada6f9e4e99e4142abb17f2bd80e0a0b4bc61
zhankq@163.com <zhankq@163.com>
2021-06-19 17:03:08 +0800

    更新内容。

---
+++ fen2_python/03/t1.py
@@ -0,0 +1,7 @@
+s = input("输入字符串，用逗号分隔:")
+
+tupless = tuple((s.split(",")))
+#无组乘以3
+news = tupless*3
+
+print(news+("fkjava","crazyit"))
---
+++ fen2_python/03/t2.py
@@ -0,0 +1,4 @@
+list1 = list(range(10))
+print(list1)
+list2 = list1[1:4]
+print(list2)
