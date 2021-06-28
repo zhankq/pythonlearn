59415d84612e9d5f40e17cd49434ab1f7b02d761
vancekq <zhankq@163.com>
2021-06-29 07:52:05 +0800

    .

--- fen2_python/PYC03-cloudms/PYC03/cloudms/cloudms/urls.py
+++ fen2_python/PYC03-cloudms/PYC03/cloudms/cloudms/urls.py
@@ -1,7 +1,7 @@
 """cloudms URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from msgapp import views
+from django.urls import include, path
+from msgapp import views as msgviews
 
 urlpatterns = [
-    path("msggate/",include("msgapp.urls")),
-    path("test/",views.test),
-    path("article/<str:year>/",views.article),
+    path('msggate/', include('msgapp.urls')),
+    path('admin/', admin.site.urls),
+    path('', msgviews.homeproc2),
+    path('playground/', msgviews.pgproc)
 
-
-    path('admin/', admin.site.urls)
 ]
--- learn/cloudms/msgapp/views.py
+++ learn/cloudms/msgapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from datetime import datetime
+from django.http import HttpResponse
+#from django.shortcuts import HttpResponse
+
 # Create your views here.
 def msgproc(request):
     datalist = []
@@ -27,5 +30,25 @@ def msgproc(request):
                         datalist.append(d)
                     if cnt >= 10:
                         break
+        else:
+            with open("msgdata.txt","r") as f:
+                cnt = 0
+                #print(f)
+                for line in f:
+                    linedata = line.split('--')
+                    cnt= cnt+1
+                    d = {"userA":linedata[1],"msg":linedata[2],"time":linedata[3]}
+                    datalist.append(d)
+                    if cnt >= 10:
+                        break
+
     return render(request,"MsgSingleWeb.html", {"data":datalist})
+
+def test(request):
+    if request.method == "GET":
+        return render(request,"MsgTest.html")
+    #    return "good index"
+
+def article(request,year):
+    return HttpResponse("this {0}".format(year))
 
